853021b0cb42ddacdf8210ed532a3638681e8249
Shazia Khan <shaziya5416@gmail.com>
2022-01-21 16:25:43 +0530

    test

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -1,1 +1,32 @@
-//
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+  }
+  
+  function isPalindrome(str) {
+    let j = str.length - 1;
+    for (let i = 0; i < j / 2; i++) {
+      let x = str[i];
+      let y = str[j - i];
+      if (x != y) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function getCircumference(radius) {
+    let result = 2 * (22 / 7) * radius;
+    return `The circumference is ${result}`;
+  }
+  
+  function getArea(radius) {
+    let result = (22 / 7) * radius * radius;
+    return `The area is ${result}`;
+  }
+  
+  module.exports = {
+    getFullName,
+    isPalindrome,
+    getCircumference,
+    getArea,
+  };
